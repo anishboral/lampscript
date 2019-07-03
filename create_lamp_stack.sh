df22197b724d0e79a9bc9a73e2c032a7b6fe1799
anishboral <47697472+anishboral@users.noreply.github.com>
2019-07-03 13:30:14 +0100

    Update create_lamp_stack.sh

--- create_lamp_stack.sh
+++ create_lamp_stack.sh
@@ -2,9 +2,10 @@ apt-get update
 apt-get install apache2 -y
 apt-get install php -y
 apt install php libapache2-mod-php -y
-apt install php7.0-mbstring -y
+apt install php-mbstring -y
 apt install php-gd -y
-apt install php-dom -y
+apt install php-gd -y
+apt install php-curl -y
 apt-get install php-mysql -y
 a2enmod rewrite
 a2enmod headers
