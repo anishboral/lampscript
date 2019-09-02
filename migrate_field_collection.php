b7b2a71987dfb18633280c3e101e815542bfa587
anishboral <47697472+anishboral@users.noreply.github.com>
2019-09-02 12:42:01 +0100

    Update migrate_field_collection.php

--- migrate_field_collection.php
+++ migrate_field_collection.php
@@ -2,10 +2,10 @@
 $db_con = mysqli_connect("localhost", "oxfordowl", "oxfordowl");
 mysqli_select_db($db_con, "drupal7");
 
-$field_collection_machine_name_array = array(
-        'field_print_incetives' => array(
-                'field_label', 'field_description'
-        ));
+$field_collection_machine_name_array = array('field_print_special_position');
+
+
+$field_collection_fields = array('field_label', 'field_description');
 
 ################################
 
@@ -21,6 +21,11 @@ mysqli_query($db_con, "CREATE TABLE $destination_table SELECT * FROM $source_tab
 mysqli_query($db_con, "ALTER TABLE `$destination_table` DROP `entity_type`");
 mysqli_query($db_con, "ALTER TABLE `$destination_table` CHANGE `language` `langcode` varchar(32) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '' COMMENT 'The language for this data item.' AFTER `revision_id`");
 
+
+
+}
+
+
 foreach($field_collection_fields as $fc_field) {
 $source_table = 'field_data_' . $fc_field;
 $destination_table = 'field_collection_item__' . $fc_field;
@@ -30,8 +35,5 @@ if (!in_array($fc_field, $keep_format_col)) {
         mysqli_query($db_con, "ALTER TABLE `$destination_table` DROP `" . $fc_field . "_format`");
 }
 mysqli_query($db_con, "ALTER TABLE `$destination_table` CHANGE `language` `langcode` varchar(32) COLLATE 'utf8_general_ci' NOT NULL DEFAULT '' COMMENT 'The language for this data item.' AFTER `revision_id`");
-
-}
-
 
 }
