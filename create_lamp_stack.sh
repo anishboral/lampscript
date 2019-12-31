75e23f97c9cf9340166e6acfbc672150e99b2387
anishboral <47697472+anishboral@users.noreply.github.com>
2019-12-31 14:07:50 +0000

    Update create_lamp_stack.sh

--- create_lamp_stack.sh
+++ create_lamp_stack.sh
@@ -5,6 +5,7 @@ apt install php libapache2-mod-php -y
 apt install php-mbstring -y
 apt install php-gd -y
 apt install php-curl -y
+apt install php-dom -y
 apt-get install php-mysql -y
 a2enmod rewrite
 a2enmod headers
@@ -12,4 +13,5 @@ service apache2 restart
 ufw allow http
 ufw allow https
 apt install zip -y
+apt-get install mysql-server -y
 echo "<?php phpinfo();" > /var/www/html/phpinfo.php
